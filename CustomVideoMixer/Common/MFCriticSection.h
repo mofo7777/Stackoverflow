04919d97f6d165b195c5e70a92a1c99b69d4720a
mofo7777 <41967574+mofo7777@users.noreply.github.com>
2018-09-03 20:46:00 +0200

    Update MFCriticSection.h

--- CustomVideoMixer/Common/MFCriticSection.h
+++ CustomVideoMixer/Common/MFCriticSection.h
@@ -5,30 +5,30 @@
 #define MFCRITICSECTION_H
 
 class CriticSection{
-
-  private:
-				
-				CRITICAL_SECTION m_Section;
-
-  public:
-				
-				CriticSection(){ InitializeCriticalSection(&m_Section); }
-				~CriticSection(){ DeleteCriticalSection(&m_Section); }
-
-				void Lock(){ EnterCriticalSection(&m_Section); }
-				void Unlock(){ LeaveCriticalSection(&m_Section); }
+	
+private:
+	
+	CRITICAL_SECTION m_Section;
+	
+public:
+	
+	CriticSection(){ InitializeCriticalSection(&m_Section); }
+	~CriticSection(){ DeleteCriticalSection(&m_Section); }
+	
+	void Lock(){ EnterCriticalSection(&m_Section); }
+	void Unlock(){ LeaveCriticalSection(&m_Section); }
 };
 
 class AutoLock{
-
-  private:
-				
-				CriticSection* m_pSection;
-
-  public:
-				
-				AutoLock(CriticSection& critic){ m_pSection = &critic; m_pSection->Lock(); }
-				~AutoLock(){ m_pSection->Unlock(); }
+	
+private:
+	
+	CriticSection* m_pSection;
+	
+public:
+	
+	AutoLock(CriticSection& critic){ m_pSection = &critic; m_pSection->Lock(); }
+	~AutoLock(){ m_pSection->Unlock(); }
 };
 
 #endif
