f9372395cd8bada09eef77e9b8c16377444bd705
mofo7777 <41967574+mofo7777@users.noreply.github.com>
2018-09-18 20:30:23 +0200

    Add files via upload

---
+++ CustomVideoMixer/Common/MFLogCommon.h
@@ -0,0 +1,177 @@
+//----------------------------------------------------------------------------------------------
+// MFLogCommon.h
+//----------------------------------------------------------------------------------------------
+#ifndef MFLOGCOMMON_H
+#define MFLOGCOMMON_H
+
+#if (_DEBUG && MF_USE_LOGGING)
+
+typedef LPCWSTR(*GetGUIDStringName)(const GUID&);
+
+inline void LogGuidHexa(const GUID& guid, const BOOL bFirst){
+
+	HRESULT hr;
+	WCHAR pBuffer[39] = {0};
+
+	hr = StringCchPrintf(pBuffer, 39, L"{%.8lX-%.4hX-%.4hX-%.2hhX%.2hhX-%.2hhX%.2hhX%.2hhX%.2hhX%.2hhX%.2hhX}",
+		guid.Data1,
+		guid.Data2,
+		guid.Data3,
+		guid.Data4[0],
+		guid.Data4[1],
+		guid.Data4[2],
+		guid.Data4[3],
+		guid.Data4[4],
+		guid.Data4[5],
+		guid.Data4[6],
+		guid.Data4[7]
+	);
+
+	if(SUCCEEDED(hr)){
+
+		if(bFirst)
+			TRACE_NO_END_LINE((L"\t%s\t", pBuffer));
+		else
+			TRACE((L"%s", pBuffer));
+	}
+	else{
+
+		TRACE((L"Guid problem"));
+	}
+}
+
+inline void LogUINT32AsUINT64(const PROPVARIANT& var){
+
+	UINT32 uHigh = 0, uLow = 0;
+
+	Unpack2UINT32AsUINT64(var.uhVal.QuadPart, &uHigh, &uLow);
+
+	TRACE((L"%u x %u", uHigh, uLow));
+}
+
+inline HRESULT SpecialCaseAttributeValue(GUID guid, const PROPVARIANT& var){
+
+	if(guid == MF_MT_FRAME_RATE){
+		LogUINT32AsUINT64(var);
+	}
+	else if(guid == MF_MT_FRAME_SIZE){
+		LogUINT32AsUINT64(var);
+	}
+	else if(guid == MF_MT_PIXEL_ASPECT_RATIO){
+		LogUINT32AsUINT64(var);
+	}
+	else{
+		return S_FALSE;
+	}
+
+	return S_OK;
+}
+
+inline void LogPropertyVariant(const GUID* pGuid, const PROPVARIANT& var, GetGUIDStringName pGetGUIDString){
+
+	HRESULT hr = S_FALSE;
+
+	if(pGuid != NULL)
+		hr = SpecialCaseAttributeValue(*pGuid, var);
+
+	if(hr == S_FALSE){
+
+		switch(var.vt){
+
+		case VT_UI4:
+			TRACE((L"%lu", var.ulVal));
+			break;
+
+		case VT_I4:
+			TRACE((L"%ld", var.lVal));
+			break;
+
+		case VT_UI8:
+			TRACE((L"%I64u", var.uhVal));
+			break;
+
+		case VT_BOOL:
+			TRACE((L"%s", var.boolVal == -1 ? "true" : "false"));
+			break;
+
+		case VT_R8:
+			TRACE((L"%f", var.dblVal));
+			break;
+
+		case VT_CLSID:
+		{
+			LPCWSTR pwszGuidName = pGetGUIDString(*var.puuid);
+
+			if(pwszGuidName != NULL){
+				TRACE((L"%s", pwszGuidName));
+			}
+			else{
+				LogGuidHexa(*var.puuid, FALSE);
+			}
+		}
+		break;
+
+		case VT_LPWSTR:
+			// Log  : var.pwszVal
+			TRACE((L"VT_LPWSTR = todo"));
+			break;
+
+		case VT_VECTOR | VT_UI1:
+			TRACE((L"(VT_VECTOR|VT_UI1) = <<byte array>>"));
+			break;
+
+		case VT_UNKNOWN:
+			TRACE((L"VT_UNKNOWN = IUnknown"));
+			break;
+
+		case VT_EMPTY:
+			TRACE((L"VT_EMPTY"));
+			break;
+
+		case VT_R4:
+			TRACE((L"VT_R4 = %f", var.fltVal));
+			break;
+
+			// 8195 = VT_ARRAY | VT_I4
+
+		default:
+			TRACE((L"Unexpected attribute type (vt = %hu)", var.vt));
+			break;
+		}
+	}
+}
+
+inline void LogAttributeValueByIndex(IMFAttributes* pAttributes, UINT32 uiIndex, GetGUIDStringName pGetGUIDString){
+
+	HRESULT hr;
+	PROPVARIANT var;
+	GUID guid = GUID_NULL;
+
+	PropVariantInit(&var);
+
+	hr = pAttributes->GetItemByIndex(uiIndex, &guid, &var);
+
+	if(SUCCEEDED(hr)){
+
+		LPCWSTR pwszGUIDNameAttributes = pGetGUIDString(guid);
+
+		if(pwszGUIDNameAttributes != NULL){
+			TRACE_NO_END_LINE((L"\t%s\t", pwszGUIDNameAttributes));
+		}
+		else{
+
+			LogGuidHexa(guid, TRUE);
+		}
+
+		LogPropertyVariant(&guid, var, pGetGUIDString);
+	}
+	else{
+		TRACE((L"\tGetItemByIndex (Index = %u)  hr = %s", uiIndex, MFErrorString(hr)));
+	}
+
+	PropVariantClear(&var);
+}
+
+#endif
+
+#endif
